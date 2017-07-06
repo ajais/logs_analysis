aa0098f0e32dafe20b72374a153dbd64b1d3172d
Alexandre Jais <ajais@fb.com>
2017-07-06 00:58:02 -0700

    Commented Script

--- logs_analysis.py
+++ logs_analysis.py
@@ -6,6 +6,12 @@ import psycopg2
 
 
 def get_pop_articles():
+    """ Joins the articles and log tables from the news database to count 
+    the number of times each article is displayed and find the 3 most popular. 
+    The articles are identified with a unique slug that also appears in the path 
+    displayed in the log table. As a result the path can be formatted 
+    to match the slug and do the join that way.
+    """
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     c.execute("select title, count(*) \
@@ -21,6 +27,11 @@ def get_pop_articles():
 
 
 def get_pop_authors():
+    """ Joins the authors, articles and log tables from the news database to count 
+    the total number of reads per author and rank them. We proceed the same way to 
+    join the log and articles table which will give us reads and instead of grouping 
+    by article title, we group everything by author.
+    """
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     c.execute("select name, count(*) \
@@ -37,20 +48,26 @@ def get_pop_authors():
 
 
 def get_days_errors():
+    """ Here we leverage the avg method to generate the occurence of errors 
+    for each day as a percentage. And then use it in a subquery to then find 
+    the days where the occurence of errors is superior than 1%.
+    """
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
     c.execute("select error_table.* \
               from (select date(time), \
               avg(case when status = '200 OK' then 0 else 1 end) as error \
-              from log group by date(time)) as error_table \
+              from log \
+              group by date(time)) as error_table \
               where error >= 0.01")
     POSTS = c.fetchall()
     db.close()
     return POSTS
 
+# Create an output file or overwrite the last created one
 f = open("output.txt", "w")
 
-
+# Output the most popular articles
 f.write('3 most popular articles of all time:\n')
 print('3 most popular articles of all time:\n')
 pop_art = get_pop_articles()
@@ -60,6 +77,7 @@ for i in pop_art:
 f.write('\n')
 print('\n')
 
+# Output the most popular authors
 f.write('Most popular authors of all time:\n')
 print('Most popular authors of all time:\n')
 pop_auth = get_pop_authors()
@@ -69,6 +87,7 @@ for i in pop_auth:
 f.write('\n')
 print('\n')
 
+# Output the days with more than 1% errors
 f.write('Days where more than 1% of requests lead to errors:\n')
 print('Days where more than 1% of requests lead to errors:\n')
 days_err = get_days_errors()
